b4c2dd0f410804448ccbd3487f201cf1d410186b
vadimor <vadimorbiluy@gmail.com>
2021-12-13 20:51:28 +0200

    Add Class 1 and Class 2

---
+++ Module3HW3/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Module3HW3
+{
+    public class Class2
+    {
+        private int _result;
+        public Func<int, bool> Calc(Func<int, int, int> pow, int x, int y)
+        {
+            _result = pow.Invoke(x, y);
+            return Result;
+        }
+
+        private bool Result(int x)
+        {
+            return _result % x == 0;
+        }
+    }
+}
--- Module3HW3/Program.cs
+++ Module3HW3/Program.cs
@@ -6,6 +6,16 @@ namespace Module3HW3
     {
         public static void Main(string[] args)
         {
+            var prog = new Program();
+            var class1 = new Class1 { Show = prog.Show };
+            var class2 = new Class2();
+            var result = class2.Calc(class1.Pow, 10, 5).Invoke(5);
+            class1.Show.Invoke(result);
+        }
+
+        public void Show(bool result)
+        {
+            Console.WriteLine(result);
         }
     }
 }
